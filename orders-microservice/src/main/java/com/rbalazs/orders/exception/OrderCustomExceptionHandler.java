fb6da0f2ac8412ac7ebd6514e74a95773cb1c63e
Rodrigo Balazs <rodrigobalazs@gmail.com>
2024-11-04 20:38:45 -0300

    removes unnecesary inheritance at Order and Stock ExceptionHandlers

--- orders-microservice/src/main/java/com/rbalazs/orders/exception/OrderCustomExceptionHandler.java
+++ orders-microservice/src/main/java/com/rbalazs/orders/exception/OrderCustomExceptionHandler.java
@@ -3,13 +3,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * Used to intercept {@link OrderCustomException}
  */
 @ControllerAdvice
-public class OrderCustomExceptionHandler extends ResponseEntityExceptionHandler {
+public class OrderCustomExceptionHandler {
 
     /**
      * Intercepts a given {@link OrderCustomException} in order to return to the view an HTTP RESPONSE with the
@@ -19,7 +18,7 @@ public class OrderCustomExceptionHandler extends ResponseEntityExceptionHandler 
      * {the entity was not found in the Order application}
      */
     @ExceptionHandler(OrderCustomException.class)
-    public ResponseEntity<Object> handleOrderCustomException(final OrderCustomException ex, final WebRequest request) {
+    public ResponseEntity<Object> handleOrderCustomException(final OrderCustomException ex) {
         return ResponseEntity.status(ex.getStatus()).body(ex.getMessage());
     }
 }
--- stock-microservice/src/main/java/com/rbalazs/stock/exception/StockCustomExceptionHandler.java
+++ stock-microservice/src/main/java/com/rbalazs/stock/exception/StockCustomExceptionHandler.java
@@ -3,7 +3,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * Used to intercept {@link StockCustomException}
@@ -11,7 +10,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  * @author Rodrigo Balazs
  */
 @ControllerAdvice
-public class StockCustomExceptionHandler extends ResponseEntityExceptionHandler {
+public class StockCustomExceptionHandler {
 
     /**
      * Intercepts a given {@link StockCustomException} in order to return to the view an HTTP RESPONSE with the exception
@@ -21,7 +20,7 @@ public class StockCustomExceptionHandler extends ResponseEntityExceptionHandler 
      * {the entity was not found in the Stock application}
      */
     @ExceptionHandler(StockCustomException.class)
-    public ResponseEntity<Object> handleStockCustomException(final StockCustomException ex, final WebRequest request) {
+    public ResponseEntity<Object> handleStockCustomException(final StockCustomException ex) {
         return ResponseEntity.status(ex.getStatus()).body(ex.getMessage());
     }
 }
